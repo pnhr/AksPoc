b20d09ec2ec777ccd8ff657945fad0c5586b2bb4
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-06-22 17:28:32 +0530

    DDDD

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -27,7 +27,7 @@ public class ProductController : ControllerBase
         {
             Id = Random.Shared.Next(1, 55),
             Name = Summaries[Random.Shared.Next(Summaries.Length)],
-            Count = 747 //Random.Shared.Next(10, 55)
+            Count = 23 //Random.Shared.Next(10, 55)
         })
         .ToArray();
     }
